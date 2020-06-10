6b214d51db7f1ebd49760bdf23bec4428c9b7488
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-06-09 22:09:19 -0300

    Welcome
    Tela de Welcome, contendo os botões de Login e Register

---
+++ Welcome.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import {
+    Text,
+    StyleSheet,
+    View,
+    TouchableOpacity
+} from 'react-native';
+import styles from '../style/WelcomeS';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default class Main extends Component
+{
+  render()
+  {
+    return(
+      <View style = {styles.masterContainer}> 
+        <LinearGradient
+          colors = {['#FFF', '#498A57']}
+          style = {styles.background}
+        >
+          <Text style = {styles.title}> GREENIER LIFE </Text>
+          <TouchableOpacity 
+            style = {styles.mainButton}
+            onPress = {() => {this.props.navigation.navigate('Login')}}
+          >
+            <Text style = {styles.textButton}> Login </Text>
+          </TouchableOpacity>
+          <TouchableOpacity 
+            style = {styles.mainButton}
+            onPress = {() => {this.props.navigation.navigate('Register')}}
+          >
+            <Text style = {styles.textButton}> Register </Text>
+          </TouchableOpacity>
+        </LinearGradient>
+      </View>
+    )
+  }
+}
