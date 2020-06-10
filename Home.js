23500cf38a3b0151b3686d2b46ceb6f6e85adfc0
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-06-09 22:08:34 -0300

    Home
    Pequenos ajustes e remoção do getParams, devido a um erro no expo.

--- Home.js
+++ Home.js
@@ -1,38 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     Text,
     StyleSheet,
     View 
 } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Profile from './Profile';
+import Store from './Store';
 
 export default class Home extends Component 
 {
-    render()
-    {
-        const {navigation} = this.props;
-        const username = navigation.getParam('name');
-
-        return (
-            <View style = {styles.container}>
-                <Text style = {styles.text}>
-                    Olá {username} !!!
-                </Text>
-                <Text style = {styles.text}> 
-                    Bem vindo a Greenier Life 
-                </Text>
-            </View>
-        );
-    }
+  render()
+  {
+    const Tab = createMaterialBottomTabNavigator();
+    return (
+      <View style = {styles.container}>
+        <Text style = {styles.text}>
+          Oi!!!
+        </Text>
+        <Text style = {styles.text}> 
+          Homepage
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    text: {
-       fontSize: 20,
-       color: '#498A57' 
-    }
-});
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 20,
+    color: '#498A57' 
+  }
+})
