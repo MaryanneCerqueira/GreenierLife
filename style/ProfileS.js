ff37fd7fe7975a97e5fee000f7b092eb0ec9d21e
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-06-16 11:20:58 -0300

    ProfileS.js
    style da página de perfil

---
+++ style/ProfileS.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  wardrobe: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  picbox: {
+    paddingTop: 30,
+    height: 150,
+    borderWidth: 4,
+    alignItems: 'center', 
+    justifyContent: 'flex-start'
+  },
+  text: {
+    color: '#498A57'
+  }
+});
+
+export default styles;
