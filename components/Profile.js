6bcf91852983b9e22fef514cb9bc0c9442cc9de1
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-06-16 22:32:53 -0300

    Profile.js
    Implementado o wardrobe, com a possibilidade do usuário mudar sua roupa

--- components/Profile.js
+++ components/Profile.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 import {
     Text,
+    StyleSheet,
     View,
-    Image
+    Image,
+    TouchableOpacity
 } from 'react-native';
 import styles from '../style/ProfileS';
 
 export default class Profile extends Component 
 {
-
+  state = {
+    pic: '../assets/aaaaaa.jpg'
+  }
   render()
   {
     return (
-      <View style = {styles.container}>
+      <View style = {styles.masterContainer}>
         <View style = {styles.picbox}>
           <Image
-            source = {require('../assets/babuzada.png')} 
+            source = {require(this.state.pic)} 
             style = {{width: 75, height: 75, borderRadius: 75}}
           />
           <Text style = {styles.text}>
@@ -23,9 +27,51 @@ export default class Profile extends Component
           </Text>
         </View>
         <View style = {styles.wardrobe}>
-          <Text style = {styles.text}>
-            Oi!!! Aqui vai ser o wardrobe
-          </Text>
+          <View style = {styles.redbox}>
+            <Image
+              source = {require('../assets/babuzada.png')} 
+              style = {{width: 75, height: 75, borderRadius: 75}}
+            />
+            <TouchableOpacity
+              onPress = {() => {
+                this.setState({
+                  pic: '../assets/babuzada.png'
+                })
+              }}
+            >
+              <Text>Usar esse</Text>
+            </TouchableOpacity> 
+          </View>
+          <View style = {styles.redbox}>
+            <Image
+              source = {require('../assets/aaaaaa.jpg')} 
+              style = {{width: 75, height: 75, borderRadius: 75}}
+            />
+            <TouchableOpacity
+              onPress = {() => {
+                this.setState({
+                  pic: '../assets/aaaaaa.jpg'
+                })
+              }}
+            >
+              <Text>Usar esse</Text>
+            </TouchableOpacity>
+          </View>
+          <View style = {styles.redbox}>
+            <Image
+              source = {require('../assets/bloodtrail.jpg')} 
+              style = {{width: 75, height: 75, borderRadius: 75}}
+            /> 
+            <TouchableOpacity
+              onPress = {() => {
+                this.setState({
+                  pic: '../assets/bloodtrail.jpg'
+                })
+              }}
+            >
+              <Text>Usar esse</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
