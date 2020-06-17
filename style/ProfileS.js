92db73e8405aa544cf3f455367b743892a8cd7be
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-06-16 22:33:34 -0300

    ProfileS.js
    Criado as caixas que ficam as roupas do usuário

--- style/ProfileS.js
+++ style/ProfileS.js
@@ -1,24 +1,34 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  masterContainer: {
     flex: 1
   },
   wardrobe: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    flexDirection: 'row'
   },
   picbox: {
     paddingTop: 30,
     height: 150,
-    borderWidth: 4,
+    borderBottomWidth: 3,
     alignItems: 'center', 
     justifyContent: 'flex-start'
   },
   text: {
     color: '#498A57'
-  }
+  },
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#FFF',
+    borderColor: '#498A57',
+    borderWidth: 2,
+    marginLeft: 1,
+    alignItems: 'center'
+   },
 });
 
 export default styles;
