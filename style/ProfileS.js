f337d13047363004efcae376c7ccff5f13f53a3e
João Lucas Simões Moreira <51100086+JNDGitHub@users.noreply.github.com>
2020-06-25 23:49:47 -0300

    style
    Grandes mudanças no estilo das respectivas páginas, com estilização do botão e conserto daquele bug do scrollview

--- style/ProfileS.js
+++ style/ProfileS.js
@@ -5,11 +5,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1
   },
-  /*wardrobeContainer: {
-    marginTop: 1,
-    alignItems: 'center',
-    flexDirection: 'column',
-  },*/
   wardrobeColumn: {
     flexDirection: 'row',
     justifyContent: 'center',
@@ -18,26 +13,52 @@ const styles = StyleSheet.create({
   pfpBox: {
     paddingTop: 30,
     height: 150,
-    borderBottomWidth: 3,
     alignItems: 'center', 
     marginBottom: 1
   },
-  text: {
+  username: {
     color: '#498A57'
   },
   clothesBox: {
     width: 150,
-    height: 150,
+    height: 'auto',
     backgroundColor: '#FFF',
     borderColor: '#498A57',
     borderWidth: 2,
-    marginBottom: 1,
-    marginLeft: 1,
+    marginLeft: 10,
+    marginBottom: 10,
+    paddingBottom: 5,
     alignItems: 'center'
    },
    clothesPic: {
     width: 100, 
     height: 100
+   },
+   header: {
+    marginTop: 30,
+    height: 60,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderBottomWidth: 2.5,
+    borderBottomColor: '#86C54E',
+    paddingBottom: 1,
+    marginBottom: 1
+   },
+   title: {
+    textAlign: 'center',
+    color: '#4E4E4E',
+    fontSize: 25,
+    fontWeight: 'bold',
+   },
+   userMoney: {
+     color: '#CCAA00'
+   },
+   wearButton: {
+    backgroundColor: '#B2DF8A',
+    marginTop: 5, 
+    borderRadius: 50,
+    paddingLeft: 30,
+    paddingRight: 30
    }
 });
 
--- style/StoreS.js
+++ style/StoreS.js
@@ -10,27 +10,46 @@ const styles = StyleSheet.create ({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  header: {
+    marginTop: 30,
+    height: 60,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderBottomWidth: 2.5,
+    borderBottomColor: '#86C54E',
+    paddingBottom: 10,
+    marginBottom: 10
+  },
   clothesBox: {
     width: 150,
     height: 150,
     backgroundColor: '#FFF',
     borderColor: '#498A57',
     borderWidth: 2,
-    marginLeft: 1,
-    marginBottom: 1,
+    marginLeft: 10,
+    marginBottom: 10,
     alignItems: 'center'
    },
   clothesPrice: {
-    fontSize: 20,
-    color: '#498A57'
+    fontSize: 12,
+    color: '#CCAA00'
    },
   clothesPic: {
     width: 100, 
     height: 100
    },
   title: {
-    color: '#498A57',
-    fontSize: 40
+    textAlign: 'center',
+    color: '#4E4E4E',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  buyButton: { 
+    backgroundColor: '#B2DF8A', 
+    borderRadius: 50,
+    paddingLeft: 30,
+    paddingRight: 30,
+    marginBottom: 2
   }
 });
 
